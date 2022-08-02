e25012419c84a2de90a0a3e5368099296655c351
Reak Under <108196667+yeomdara@users.noreply.github.com>
2022-08-02 14:23:56 +0700

    first commit

---
+++ build/precache-manifest.8ecd80167eaf48faf3d21bc67ed731c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe448ce34e39a5c0982c",
+    "url": "/my-app/static/css/main.6ffcf921.chunk.css"
+  },
+  {
+    "revision": "fe448ce34e39a5c0982c",
+    "url": "/my-app/static/js/main.fe448ce3.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/my-app/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "2b806fdf09449f5ff5d26b0e3df0cc13",
+    "url": "/my-app/index.html"
+  }
+];
